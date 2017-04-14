ed2d27d546b83fc75b3bf14250eda17ef90348c2
king113 <caid113@qq.com>
2017-04-14 10:47:26 +0800

    init commit

---
+++ Hello/src/main/java/hello.java
@@ -0,0 +1,9 @@
+/**
+ * Created by ¿­¸ç on 2017/4/13.
+ */
+public class hello {
+        public static void main(String[] args) {
+                System.out.println("Hello Word!");
+
+        }
+}
